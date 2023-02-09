d2edddf1a4609f46d1f07a272435aaf763dbcb74
Gabriel <gnicholauson@gmail.com>
2023-02-09 17:26:57 -0600

    added movie to rating

--- Models/Rating.cs
+++ Models/Rating.cs
@@ -5,11 +5,15 @@
         private User _user;
         public User User { get { return _user; } }
 
+        private Movie _movie;
+        public Movie Movie { get { return _movie; } }   
+
         private double _score;
         public double Score { get { return _score; } }    
-        public Rating(User user, double score) 
+        public Rating(User user, Movie movie, double score) 
         {
             _user = user;
+            _movie = movie;
             _score = score;
         } 
     }
