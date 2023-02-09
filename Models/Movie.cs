65445e20b28796c22ba9b954b98f8b900e64d04c
Gabriel <gnicholauson@gmail.com>
2023-02-09 15:11:44 -0600

    add movie model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -2,5 +2,24 @@
 {
     public class Movie
     {
+        private string _name;
+        public string Name { get { return _name; } }
+
+        private string _genre;
+        public string Genre { get { return _genre; } }
+
+        private int _releaseYear;
+        public int ReleaseYear { get { return _releaseYear; } }
+
+        private int _budget;
+        public int Budget { get { return _budget; } }
+
+        public Movie(string name, string gnere, int year, int budget)
+        {
+            _name = name;
+            _genre = gnere;
+            _releaseYear = year;
+            _budget = budget;
+        }
     }
 }
