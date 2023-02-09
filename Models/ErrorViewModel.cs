a4b226af32c7094335f8876feb2df06c3d011baa
Gabriel <gnicholauson@gmail.com>
2023-02-09 15:03:47 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroToLINQAndASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
