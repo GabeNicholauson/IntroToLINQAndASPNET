e036e435a77275aa9c33cabd5d730719031f06b5
Gabriel <gnicholauson@gmail.com>
2023-02-09 15:28:40 -0600

    add movies to centext

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,6 +1,17 @@
-﻿namespace IntroToLINQAndASPNET.Data
+﻿using IntroToLINQAndASPNET.Models;
+
+namespace IntroToLINQAndASPNET.Data
 {
-    public class Context
+    public static class Context
     {
+        public static List<Movie> movies = new List<Movie>()
+        {
+            new Movie("Jurassic Park", "Action", 1993, 63_000_000),
+            new Movie("Dumb and Dumber", "Comedy", 1994, 17_000_000),
+            new Movie("The SpongeBob SquarePants Movie", "Comedy", 2004, 30_000_000),
+            new Movie("Avatar", "Action", 2009, 237_000_000),
+            new Movie("2001: A Space Odyssey", "Science Fiction", 1968, 10_500_000)
+        };
+
     }
 }
