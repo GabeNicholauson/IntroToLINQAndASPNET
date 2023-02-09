babbaa116829483464e80d1d83ea7f3fbf4fc8f8
Gabriel <gnicholauson@gmail.com>
2023-02-09 17:29:11 -0600

    add User

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,18 @@
 {
     public class User
     {
-        private 
+        private string _name;
+        public string Name { get { return _name; } }
+
+        private int _id;
+        public int Id { get { return _id; } }
+
+        public List<Rating> _allRatings = new List<Rating>();
+
+        public User(string name, int id)
+        {
+            _name = name;
+            _id = id;
+        }
     }
 }
