ba4d5d7227411b2ea82e59ed8c5ab568a8b130ce
Gabriel <gnicholauson@gmail.com>
2023-02-09 16:08:47 -0600

    can get how many movies are in a genre

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,7 +10,7 @@ namespace IntroToLINQAndASPNET.Controllers
         {
             List<Movie> allMovies = new List<Movie>();
             allMovies.Add(Context.Movies.FirstOrDefault(x => x.Name.ToLower() == name.ToLower().Trim()));
-            return View("Index", allMovies);
+            return View("Details", allMovies);
         }
 
         public IActionResult GetAllInGenre(string genre)
@@ -19,12 +19,21 @@ namespace IntroToLINQAndASPNET.Controllers
             {
                 return x.Genre.ToLower() == genre.ToLower().Trim();
             }).ToList();
-            return View("Index", allMovies);
+            return View("Details", allMovies);
         }
 
-        public void GetCountInGenre()
+        public IActionResult GetCountInGenre(string genre)
         {
-
+            int count = 0;
+            string genreCount;
+            foreach(Movie movie in Context.Movies)
+            {
+                if (movie.Genre.ToLower() == genre.ToLower().Trim()) {
+                    count++;
+                }
+            }
+            genreCount = $"{genre}: {count}";
+            return View("Count", genreCount);
         }
 
         public IActionResult MoviesInBudget(int budget)
@@ -33,12 +42,16 @@ namespace IntroToLINQAndASPNET.Controllers
             {
                 return x.Budget <= budget;
             }).ToList();
-            return View("Index", allMovies);
+            return View("Details", allMovies);
         }
 
-        public void MoviesInThe90s()
+        public IActionResult MoviesInThe90s()
         {
-
+            List<Movie> allMovies = Context.Movies.Where(x =>
+            {
+                return x.ReleaseYear >= 1990 && x.ReleaseYear < 2000;
+            }).ToList();
+            return View("Details", allMovies);
         }
 
         public void CalculateOverallRating()
