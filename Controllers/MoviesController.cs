686e3510836d472e296973e6dd056ae90da48cbc
Gabriel <gnicholauson@gmail.com>
2023-02-09 15:18:07 -0600

    add methods to MoviesController

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,6 +4,36 @@ namespace IntroToLINQAndASPNET.Controllers
 {
     public class MoviesController : Controller
     {
+        public void GetMovieInfo()
+        {
+
+        }
+
+        public void GetAllInGenre()
+        {
+
+        }
+
+        public void GetCountInGenre()
+        {
+
+        }
+
+        public void MoviesInBudget()
+        {
+
+        }
+
+        public void MoviesInThe90s()
+        {
+
+        }
+
+        public void CalculateOverallRating()
+        {
+
+        }
+
         public IActionResult Index()
         {
             return View();
