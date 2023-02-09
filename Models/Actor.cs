67b1d536873a05c662225a14ca94bf57792221ee
Gabriel <gnicholauson@gmail.com>
2023-02-09 15:14:20 -0600

    add actor model

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -2,5 +2,16 @@
 {
     public class Actor
     {
+        private string _name;
+        public string Name { get { return _name; } }
+
+        private int _salary;
+        public int Salary { get { return _salary; } }
+
+        public Actor(string name, int salary)
+        {
+            _name = name;
+            _salary = salary;
+        }
     }
 }
