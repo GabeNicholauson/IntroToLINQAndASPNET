8bbfdcf11284e854957a8ac5a2cf473cbb555974
Gabriel <gnicholauson@gmail.com>
2023-02-09 20:08:41 -0600

    re organized context

--- Data/Context.cs
+++ Data/Context.cs
@@ -7,59 +7,39 @@ namespace IntroToLINQAndASPNET.Data
     {
         private static int _id = 1;
 
-        private static Movie _jurrasicPark = new Movie("Jurassic Park", "Action", 1993, 63_000_000);
-        private static Movie _dumbAndDumber = new Movie("Dumb and Dumber", "Comedy", 1994, 17_000_000);
-        private static Movie _theSpongBobSquarePantsMovie = new Movie("The SpongeBob SquarePants Movie", "Comedy", 2004, 30_000_000);
-        private static Movie _avatar = new Movie("Avatar", "Action", 2009, 237_000_000);
-        private static Movie _2001ASpaceOdyssey = new Movie("2001: A Space Odyssey", "Science Fiction", 1968, 10_500_000);
-
-        private static User _joe = new User("Joe", _id++);
-        private static User _mary = new User("Mary", _id++);
-        private static User _harold = new User("Harold", _id++);
-
-        private static Rating _rating1 = new Rating(_joe, _avatar, 8.4);
-        private static Rating _rating2 = new Rating(_joe, _dumbAndDumber, 99.9);
-        private static Rating _rating3 = new Rating(_mary, _avatar, 33.2);
-        private static Rating _rating4 = new Rating(_harold, _jurrasicPark, 50);
-        private static Rating _rating5 = new Rating(_joe, _2001ASpaceOdyssey, 40);
-
-        private static Actor _billy = new Actor("Billy", 900_002);
-        private static Actor _kelly = new Actor("Kelly", 900_000);
-        private static Actor _quart = new Actor("Quart", 900_001);
-
         private static List<Movie> _movies = new List<Movie>()
         {
-            _jurrasicPark,
-            _dumbAndDumber,
-            _theSpongBobSquarePantsMovie,
-            _avatar,
-            _2001ASpaceOdyssey
+            new Movie("Jurassic Park", "Action", 1993, 63_000_000),
+            new Movie("Dumb and Dumber", "Comedy", 1994, 17_000_000),
+            new Movie("The SpongeBob SquarePants Movie", "Comedy", 2004, 30_000_000),
+            new Movie("Avatar", "Action", 2009, 237_000_000),
+            new Movie("2001: A Space Odyssey", "Science Fiction", 1968, 10_500_000)
         };
         public static List<Movie> Movies { get { return _movies; } }
 
         private static List<User> _users = new List<User>()
         {
-            _joe,
-            _mary,
-            _harold
+            new User("Joe", _id++),
+            new User("Mary", _id++),
+            new User("Harold", _id++)
         };
         public static List<User> Users { get { return _users; } }
 
         private static List<Rating> _ratings = new List<Rating>()
         {
-            _rating1,
-            _rating2,
-            _rating3,
-            _rating4,
-            _rating5
+            new Rating(_users.First(u => u.Name == "Joe"), _movies.First(m => m.Name == "Avatar"), 8.4),
+            new Rating(_users.First(u => u.Name == "Joe"), _movies.First(m => m.Name == "Dumb and Dumber"), 99.9),
+            new Rating(_users.First(u => u.Name == "Mary"), _movies.First(m => m.Name == "Avatar"), 33.2),
+            new Rating(_users.First(u => u.Name == "Harold"), _movies.First(m => m.Name == "Jurassic Park"), 50),
+            new Rating(_users.First(u => u.Name == "Joe"), _movies.First(m => m.Name == "2001: A Space Odyssey"), 40)
         };
         public static List<Rating> Ratings { get { return _ratings; } }
 
         private static List<Actor> _actors = new List<Actor>()
         {
-            _billy,
-            _kelly,
-            _quart
+            new Actor("Billy", 900_002),
+            new Actor("Kelly", 900_000),
+            new Actor("Quart", 900_001)
         };
         public static List<Actor> Actors { get { return _actors; } }
 
